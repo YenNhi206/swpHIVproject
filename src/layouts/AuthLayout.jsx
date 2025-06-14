7ec837dda4c33ec74ec7c00fe4ae9a9ccd80faf9
tramtb <tramtbse181958@fpt.edu.vn>
2025-06-14 16:55:13 +0700

    update navbar

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React from 'react';
+import Navbar from '../components/Navbar'; // Hoặc NewNavbar
+import Breadcrumb from '../components/Breadcrumb';
 
 export default function AuthLayout({ children }) {
-  return <div className="bg-gray-100 min-h-screen p-4">{children}</div>;
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Breadcrumb />
+      <main className="max-w-7xl mx-auto p-6">{children}</main>
+    </div>
+  );
 }
-
